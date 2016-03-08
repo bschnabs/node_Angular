35d7ab50f6b5a4c5f0e43a77175e6133b8407393
Bob Schnable <rschnable@saberlogic.com>
2016-03-07 22:44:48 -0500

    created LoginFactory.js with promise for logging in Please enter the commit message for your changes. Lines starting

---
+++ Scripts/Controllers/BaseController.js
@@ -0,0 +1,5 @@
+var BaseController = function($scope){
+  $scope.helloAgain = "This is awesome and it's Angular!";
+}
+
+BaseController.$inject = ['$scope'];
---
+++ Scripts/Node_Angular_App.js
@@ -0,0 +1,4 @@
+var Node_Angular_App = angular.module('Node_Angular_App', []);
+
+Node_Angular_App.controller('BaseController', BaseController);
+Node_Angular_App.service('SessionService', SessionService);
